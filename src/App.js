4cb54c7bf886fe13fb0893953f54aec1146874b3
Viktor <shcheglov.viktor.97@gmail.com>
2020-07-31 19:11:15 -0400

    trying to make it fit screen

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ProfilePage from './components/ProfilePage';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Grid from '@material-ui/core/Grid';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: 1000
+      size: 1000,
+      testDive: document.getElementById("bot")
     };
   }
   updateDimensions() {
     let update_height = window.innerHeight;
     this.setState({size:update_height});
+    console.log(this.state.testDive)
     }
   componentDidMount() {
     this.updateDimensions();
@@ -25,17 +28,23 @@ class App extends React.Component {
     window.removeEventListener("resize", this.updateDimensions.bind(this));
   }
   render(){
+    const { testDive,setTest} = this.state;
     return (
         <div className="App" >
           <div id="background" style={{
-          backgroundImage: 'url("https://images5.alphacoders.com/329/329713.jpg")',
-          backgroundAttachment: "fixed"}}>
-          <Router>
-            <CssBaseline />
-            <NavBar />
-            <Route path="/" component={ProfilePage}/>
-            <BotBar />
-          </Router>
+            backgroundImage: 'url("https://images5.alphacoders.com/329/329713.jpg")',
+            backgroundAttachment: "fixed",
+            backgroundPosition:"center",
+            backgroundRepeat:"no-repeat",
+            backgroundSize:"cover",
+            }}>
+            <Router>
+              <CssBaseline />
+              <NavBar />
+              <Route path="/" component={ProfilePage}/>
+              <BotBar />
+              <div style={{height:`${this.state.size-425}px`}} />
+            </Router>
           </div>
       </div>
     );
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -28,7 +28,7 @@ const LoginControl =(props)=>  {
     setTech(!showTech);
   }
 const Results = () => (
-  <Box style={{background:"Aqua",color:"white"}} clone>
+  <Box style={{background:"Aqua",color:"white",cursor:"auto"}} clone>
     <div>
     <h3>Travelers | Software Enginner and Architecture Intern |May 2020 - September 2020| Hartford,CT</h3>
     <ul style={{textAlign:"left"}}>
@@ -113,9 +113,9 @@ const Results2 = () => (
 
       return (
         <>
-        <div width="100%">
-            <h1 style={{marginTop:"0px",fontSize: "50px",fontWeight: "bold",fontFamily:"Comic Sans MS",fontStyle: "italic",color:"white"}}>Viktor Shcheglov Full Stack Developer</h1>
-            <h2 style={{marginTop:"0px",fontSize: "26px",fontFamily:"Raleway",color:"white"}}>
+        <div>
+            <h1 style={{marginTop:"0px",fontSize: "3rem",fontWeight: "bold",fontFamily:"Comic Sans MS",fontStyle: "italic",color:"darkred"}}>Viktor Shcheglov Full Stack Developer</h1>
+            <h2 style={{marginTop:"0px",fontSize: "1.5rem",fontFamily:"Raleway",color:"darkred"}}>
                 Welcome to my site! I am a college student proficient in Python, React.js and Node.js seaking an Internship for the 2021 Summer.
                 <br></br>I have previously worked for two summers in various companies in Software Development and Architecture Intern positions.
                 <br></br>You can find my complete information in the sections bellow or my most relevant expirience in my <a style={{color:"Red",textDecoration: "none"}} rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1gkiw7W3PciwtSTQI7wSkiJPSy7kZDRIm/view?usp=sharing">Resume.</a>
@@ -124,16 +124,15 @@ const Results2 = () => (
         </div>
         <div width="100%" style={{display: 'flex',justifyContent: 'center'}}>
             <Breadcrumbs aria-label="breadcrumb" style={{color:"white",fontSize:"16px"}}>
-                <Button  style={{color:"white",fontSize:"11px"}} variant="contained" color="secondary" target="_blank" onClick={onClickExp}><BusinessIcon color="white" style={{marginRight:"3px"}}/>Experience</Button>
-                <Button  style={{color:"white",fontSize:"11px"}} variant="contained" color="secondary" target="_blank" onClick ={onClickEdu}><SchoolIcon color="white" style={{marginRight:"3px"}}/>Education</Button>
-                <Button  style={{color:"white",fontSize:"11px"}} variant="contained" color="secondary" target="_blank" onClick={onClickTech}><BuildIcon color="white" style={{marginRight:"3px"}}/>Tech Skills</Button>
+                <Button  style={{color:"white",fontSize:".75rem"}} variant="contained" color="secondary" target="_blank" onClick={onClickExp}><BusinessIcon color="white" style={{marginRight:"3px"}}/>Experience</Button>
+                <Button  style={{color:"white",fontSize:".75rem"}} variant="contained" color="secondary" target="_blank" onClick ={onClickEdu}><SchoolIcon color="white" style={{marginRight:"3px"}}/>Education</Button>
+                <Button  style={{color:"white",fontSize:".75rem"}} variant="contained" color="secondary" target="_blank" onClick={onClickTech}><BuildIcon color="white" style={{marginRight:"3px"}}/>Tech Skills</Button>
             </Breadcrumbs>
         </div>
         <div>
-        <p style = {{color:"white"}}>{showExp ? <Results/> : null}</p>
+        <p style = {{color:"white",cursor:"auto"}}>{showExp ? <Results/>  : null}</p>
         <p style = {{color:"white"}}>{showEdu ? <Results1/> : null}</p>
         <p style = {{color:"white"}}>{showTech ? <Results2/> : null}</p>
-        <Test />
         </div>
         </>
       );
